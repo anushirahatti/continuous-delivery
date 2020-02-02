6e8cb6e8a61deb452dcbc67bacb2f90d22461ecc
Aniruddha Shirahatti <aniruddhashirahatti@yahoo.com>
2020-02-01 21:51:39 -0500

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -1,24 +1,10 @@
-#push
-from flask import Flask
-from flask import jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
-
-@app.route('/name/<value>')
-def name(value):
-    val = {"value": value}
-    return jsonify(val)
-
-@app.route('/bob')
-def bob():
-    val = {"value": "bob"}
-    return jsonify(val)
+import main
 
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Hello World' in r.data.decode('utf-8')
