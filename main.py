c80d1faa3edf6e3716db049eede2ac96f96984a2
anushirahatti <aniruddhashirahatti@yahoo.com>
2020-02-02 19:20:56 -0500

    pushing a build trigger

--- main.py
+++ main.py
@@ -7,23 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello I like to make AI Apps'
-
-@app.route('/hey')
-def hey():
-    """Return a friendly HTTP greeting."""
-    return 'Continuous Delivery on GCP Success!'
+    return '**Hello Aniruddha!**'
 
 @app.route('/name/<value>')
 def name(value):
     val = {"value": value}
     return jsonify(val)
-
-@app.route('/bob')
-def bob():
-    val = {"value": "bob"}
-    return jsonify(val)
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
